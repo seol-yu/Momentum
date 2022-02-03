363da92291de6d78d0ab5335c561a6a772675029
seolyu ♪ *҉＼( ˆᴗˆ )ﾉ✩ <seolyu.tjfdb@gmail.com>
2022-02-03 23:56:54 +0900

    Month modify

--- js/time.js
+++ js/time.js
@@ -4,7 +4,7 @@ const clock = document.querySelector("#seolyu__header--time--clock");
 function getTime() {
   const date = new Date();
   const year = String(date.getFullYear());
-  const month = String(date.getMonth());
+  const month = String(date.getMonth() + 1);
   const day = String(date.getDate());
   calender.innerText = `${year}년 ${month}월 ${day}일`;
   const hours = String(date.getHours()).padStart(2, "0");
